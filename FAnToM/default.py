854facc2ec7d98b8d456e34a859d8bcd1102c1e7
thilobru <thilo.brummerloh@gmail.com>
2021-11-16 20:28:05 +0100

    House with Dach and Antenne

--- FAnToM/default.py
+++ FAnToM/default.py
@@ -6,9 +6,9 @@
 ################################################################
 ###                  Reset GUI                               ###
 ################################################################
-fantom.ui.setCamera( 0, fantom.ui.Camera( fantom.math.Vector3(8.25407, -5.11767, 7.28673), fantom.math.Vector3(-3.33786e-05, 1.90735e-05, -1.95503e-05), fantom.math.Vector3(0.385908, 0.901477, 0.195993), 1, 1.0472 ) )
+fantom.ui.setCamera( 0, fantom.ui.Camera( fantom.math.Vector3(9.49728, 1.25694, 4.78729), fantom.math.Vector3(-1.12067, -0.0493129, -0.954852), fantom.math.Vector3(-0.0917472, 0.994178, -0.0565087), 1, 1.0472 ) )
 fantom.ui.setCamera( 1, fantom.ui.Camera( fantom.math.Vector3(12.1415, 0, 0), fantom.math.Vector3(0, 0, 0), fantom.math.Vector3(0, 0, 1), 0, 1.0472 ) )
-fantom.ui.setCamera( 2, fantom.ui.Camera( fantom.math.Vector3(0, -12.1415, 0), fantom.math.Vector3(0, -2.86102e-06, 0), fantom.math.Vector3(0, 0, 1), 0, 1.0472 ) )
+fantom.ui.setCamera( 2, fantom.ui.Camera( fantom.math.Vector3(0, -12.1415, 0), fantom.math.Vector3(0, -6.67572e-06, 0), fantom.math.Vector3(0, 0, 1), 0, 1.0472 ) )
 fantom.ui.setCamera( 3, fantom.ui.Camera( fantom.math.Vector3(0, 0, 12.1415), fantom.math.Vector3(0, 0, 0), fantom.math.Vector3(0, 1, 0), 0, 1.0472 ) )
 
 fantom.ui.setClippingPlane( fantom.ui.ClippingPlane( 0, fantom.math.Vector4( 1, 0, 0, 1 ), False ) )
@@ -20,7 +20,7 @@ fantom.ui.setClippingPlane( fantom.ui.ClippingPlane( 5, fantom.math.Vector4( -0,
 
 fantom.ui.setBackgroundColor( fantom.math.Color(0, 0, 0, 1) )
 
-fantom.ui.setRotationCenter( fantom.ui.RotationCenter( fantom.math.Vector3(0, 0, 0), True, True, True ) )
+fantom.ui.setRotationCenter( fantom.ui.RotationCenter( fantom.math.Vector3(-0.0395294, 0, 0.828563), True, True, True ) )
 
 
 ################################################################
@@ -29,8 +29,8 @@ fantom.ui.setRotationCenter( fantom.ui.RotationCenter( fantom.math.Vector3(0, 0,
 Tasks_TaskTest = fantom.makeAlgorithm("Tasks/TaskTest")
 Tasks_TaskTest.setName("Tasks/TaskTest")
 Tasks_TaskTest.setAutoSchedule(True)
-Tasks_TaskTest.setOption("nHouses", 4)
-Tasks_TaskTest.setOption("durchschnittHeight", 5)
+Tasks_TaskTest.setOption("nHouses", 1)
+Tasks_TaskTest.setOption("nlevels", 5)
 fantom.ui.setAlgorithmPosition(Tasks_TaskTest, fantom.math.Vector2(0, 218.4))
 
 # Inbound connections of this algorithm:
--- FAnToM/grundaufgaben_src/plugin1/task1Test.cpp
+++ FAnToM/grundaufgaben_src/plugin1/task1Test.cpp
@@ -19,9 +19,9 @@ namespace
             Options( fantom::Options::Control& control )
                 : DataAlgorithm::Options( control )
             {
-                add< int > ( "nHouses", "", 4);
+                add< int > ( "nHouses", "", 1);
                 addSeparator();
-                add< int >( "durchschnittHeight", "", 5 );
+                add< int >( "nlevels", "", 5 );
             }
         };
  
@@ -39,17 +39,39 @@ namespace
             : DataAlgorithm( data )
         {
         }
-        static void makeHouse(std::vector<Point<3>>& points,
-                              std::vector<size_t>& indexes){
-            int levels = 3;
-            for(double i = 0; i < levels+1; i++) {
-                points.push_back({0,i,0});
-                points.push_back({1,i,0});
-                points.push_back({1,i,1});
-                points.push_back({0,i,1});
+        static void makeCoords(std::vector<std::vector<double>>& coords,
+                               int& nHouses){
+            std::random_device dev;
+            std::mt19937 rng(dev());
+            std::uniform_int_distribution<std::mt19937::result_type> dist(-(std::sqrt(nHouses)),std::sqrt(nHouses)); // distribution in range [-anzahlHäuser/2, anzahlHäuser/2]
+            std::cout << dist(rng) << std::endl;
+
+            for(int i = 0; i < nHouses; i++) {
+                redo:;
+                std::vector <double> v1;
+                for(short j = 0; j < 2; j++) {
+                    v1.push_back(dist(rng));
+                }
+                for(int j = 0; j < (int) coords.size()-1; j++) {
+                    if(v1[0] == coords[j][0] && v1[1] == coords[j][1]){
+                        goto redo;
+                    }
+                }
+                coords.push_back(v1);
             }
-            points.push_back({0.5, levels + 1.0, 0.5});
-            points.push_back({0.5, levels + 2.0, 0.5});
+        }
+        static void makeHouse(std::vector<Point<3>>& points,
+                              std::vector<size_t>& indexes,
+                              int& levels,
+                              std::vector<double>& coords){
+            for(double i = 0; i < levels+1; i++) {
+                points.push_back({coords[0]    ,i,coords[1]});
+                points.push_back({coords[0] + 1,i,coords[1]});
+                points.push_back({coords[0] + 1,i,coords[1] + 1});
+                points.push_back({coords[0]    ,i,coords[1] + 1});
+            }
+            points.push_back({coords[0] + 0.5, levels + 1.0, coords[1] + 0.5});
+            points.push_back({coords[0] + 0.5, levels + 2.0, coords[1] + 0.5});
             //Geschosse (Tetrahedron)
             for(int i = 0; i < levels; i++){
                 for(int j = 0; j < 4; j++){
@@ -59,24 +81,35 @@ namespace
                     indexes.push_back(7-j+4*i);
                 }
             }
-            //Dach (Pyramid)
-            for(int i = points.size() - 5; i < points.size(); i++){
+            // Dach (Pyramid)
+            for(int i = points.size() - 6; i < (int) points.size() - 1; i++){
+                indexes.push_back(i);
+            }
+            // Antenne (Line)
+            for(int i = points.size() - 2; i < (int) points.size(); i++){
                 indexes.push_back(i);
             }
         }
         virtual void execute( const Algorithm::Options& options, const volatile bool& /*abortFlag*/ ) override
         {
             int nHouses = options.get<int>("nHouses");
-            std::vector < Point< 3 > > points = {};
-            size_t numCellTypes = (size_t) 1;
-            const std::pair< Cell::Type, size_t > cellCounts[] = {std::make_pair(Cell::Type::HEXAHEDRON, (size_t) 2)};
-            std::vector< size_t > indexes = {};            
+            int levels = options.get<int>("nlevels");
+            std::vector<std::vector<double>> coords;
+            makeCoords(coords, nHouses);
 
             std::shared_ptr< const Grid< 3 > > grid;
             std::shared_ptr< DataObjectBundle > bundle = std::make_shared< DataObjectBundle >();
-            
             for(int i = 0; i < nHouses; i++) {
-                makeHouse(points, indexes);
+                std::vector < Point< 3 > > points;
+                size_t numCellTypes = (size_t) 3;
+                const std::pair< Cell::Type, size_t > cellCounts[] 
+                            = {std::make_pair(Cell::Type::HEXAHEDRON, (size_t) levels), 
+                               std::make_pair(Cell::Type::PYRAMID, (size_t) 1),
+                               std::make_pair(Cell::Type::LINE, (size_t) 1)};
+                std::vector< size_t > indexes;
+                makeHouse(points, indexes, levels, coords[i]);
+                infoLog() << "Number of Points: " << points.size() << std::endl;
+                infoLog() << "Number of Indices: " << indexes.size() << std::endl;
                 grid = DomainFactory::makeGrid( points, numCellTypes, cellCounts, indexes);
                 bundle->addContent(grid);
             }
