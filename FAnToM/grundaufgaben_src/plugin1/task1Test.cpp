26c9ed89ecec34f52b30807cfd7c52dc4e217472
thilobru <thilo.brummerloh@gmail.com>
2021-11-16 12:45:12 +0100

    after IR

--- FAnToM/grundaufgaben_src/plugin1/task1Test.cpp
+++ FAnToM/grundaufgaben_src/plugin1/task1Test.cpp
@@ -39,29 +39,45 @@ namespace
             : DataAlgorithm( data )
         {
         }
-        
-        virtual void execute( const Algorithm::Options& options, const volatile bool& /*abortFlag*/ ) override
-        {
-            std::vector < Point< 3 > > points = {};
-            size_t numCellTypes = (size_t) 1;
-            const std::pair< Cell::Type, size_t > cellCounts[] = {std::make_pair(Cell::Type::HEXAHEDRON, (size_t) 8)};
-            std::vector< size_t > indices = {};            
-
-            std::shared_ptr< const Grid< 3 > > grid;
-            std::shared_ptr< DataObjectBundle > bundle = std::make_shared< DataObjectBundle >();
-            for(double i = 0; i < 2; i++) {
+        static void makeHouse(std::vector<Point<3>>& points,
+                              std::vector<size_t>& indexes){
+            int levels = 3;
+            for(double i = 0; i < levels+1; i++) {
                 points.push_back({0,i,0});
                 points.push_back({1,i,0});
                 points.push_back({1,i,1});
                 points.push_back({0,i,1});
             }
-            for(int i = 0; i < 1; i++){
-                for(int j = 0; j < 8; j++){
-                    indices.push_back(j+4*i);
+            points.push_back({0.5, levels + 1.0, 0.5});
+            points.push_back({0.5, levels + 2.0, 0.5});
+            //Geschosse (Tetrahedron)
+            for(int i = 0; i < levels; i++){
+                for(int j = 0; j < 4; j++){
+                    indexes.push_back(j+4*i);
+                }
+                for(int j = 0; j < 4; j++) {
+                    indexes.push_back(7-j+4*i);
                 }
             }
-            for(int i = 0; i < 1; i++) {
-                grid = DomainFactory::makeGrid( points, numCellTypes, cellCounts, indices);
+            //Dach (Pyramid)
+            for(int i = points.size() - 5; i < points.size(); i++){
+                indexes.push_back(i);
+            }
+        }
+        virtual void execute( const Algorithm::Options& options, const volatile bool& /*abortFlag*/ ) override
+        {
+            int nHouses = options.get<int>("nHouses");
+            std::vector < Point< 3 > > points = {};
+            size_t numCellTypes = (size_t) 1;
+            const std::pair< Cell::Type, size_t > cellCounts[] = {std::make_pair(Cell::Type::HEXAHEDRON, (size_t) 2)};
+            std::vector< size_t > indexes = {};            
+
+            std::shared_ptr< const Grid< 3 > > grid;
+            std::shared_ptr< DataObjectBundle > bundle = std::make_shared< DataObjectBundle >();
+            
+            for(int i = 0; i < nHouses; i++) {
+                makeHouse(points, indexes);
+                grid = DomainFactory::makeGrid( points, numCellTypes, cellCounts, indexes);
                 bundle->addContent(grid);
             }
             setResult("settlement", bundle);
